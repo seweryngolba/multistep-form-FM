4d881e0fd574dfcaa241b35c17a08c6f28953a06
Seweryn Golba <sgoolb@gmail.com>
2023-11-13 19:51:12 +0100

    summary step js

--- index.js
+++ index.js
@@ -20,18 +20,26 @@ const priceMapping = {
   pro: 15,
 };
 
+let selectedPlan = "";
+let isYearlyBilling = false;
+
 plans.forEach((plan) => {
   plan.addEventListener("click", function () {
+    selectedPlan = plan.classList[0];
     plans.forEach((p) => p.classList.remove("selected"));
     plan.classList.add("selected");
+    updateSummaryPlan();
+    updateTotalPrice();
   });
 });
 
 switchInput.addEventListener("change", function () {
-  const textColorMonthly = switchInput.checked
+  isYearlyBilling = switchInput.checked;
+
+  const textColorMonthly = isYearlyBilling
     ? "hsl(231, 11%, 63%)"
     : "hsl(213, 96%, 18%)";
-  const textColorYearly = switchInput.checked
+  const textColorYearly = isYearlyBilling
     ? "hsl(213, 96%, 18%)"
     : "hsl(231, 11%, 63%)";
 
@@ -39,7 +47,7 @@ switchInput.addEventListener("change", function () {
   yearlyLabel.style.color = textColorYearly;
 
   freeMonths.forEach((freeMonth) => {
-    if (switchInput.checked) {
+    if (isYearlyBilling) {
       freeMonth.style.display = "flex";
     } else {
       freeMonth.style.display = "none";
@@ -48,30 +56,216 @@ switchInput.addEventListener("change", function () {
 
   plans.forEach((plan) => {
     const planType = plan.classList[0];
+    const planTitleElement = plan.querySelector(".plan-title");
     const priceElement = plan.querySelector(".pricing");
     const freeMonthsElement = plan.querySelector(".free-months");
 
-    if (switchInput.checked) {
-      priceElement.textContent = `$${priceMapping[planType] * 10}/yr`;
-      freeMonthsElement.textContent = "2 months free";
+    const billingFrequency = isYearlyBilling ? "yr" : "mo";
+    priceElement.textContent = isYearlyBilling
+      ? `$${priceMapping[planType] * 10} / ${billingFrequency}`
+      : `$${priceMapping[planType]} / ${billingFrequency}`;
+    freeMonthsElement.textContent = "2 months free";
+  });
+
+  updateAddonPrices();
+  updateTotalPrice();
+});
+
+function updateAddonPrices() {
+  const addons = document.querySelectorAll(".add-on");
+  addons.forEach((addOn) => {
+    const extrasElement = addOn.querySelector(".extras");
+    const currentPrice = parseFloat(extrasElement.dataset.basePrice);
+
+    const billingFrequencyText = isYearlyBilling ? "/yr" : "/mo";
+    extrasElement.textContent = `+$${
+      isYearlyBilling ? currentPrice * 10 : currentPrice
+    }${billingFrequencyText}`;
+  });
+}
+
+function updateSummaryPlan() {
+  const summaryPlanContainer = document.querySelector(".summary-step");
+  const summaryPlanTitle = summaryPlanContainer.querySelector(".plan-title");
+  const summaryPlanPrice = summaryPlanContainer.querySelector(".plan-price");
+
+  if (summaryPlanTitle && selectedPlan !== null && selectedPlan !== undefined) {
+    const selectedPlanFormatted =
+      selectedPlan.charAt(0).toUpperCase() + selectedPlan.slice(1);
+
+    const billingFrequency = isYearlyBilling ? "Yearly" : "Monthly";
+    summaryPlanTitle.textContent = `${selectedPlanFormatted} (${billingFrequency})`;
+
+    updateSummaryPlanPrice();
+    updateTotalPrice();
+  }
+}
+
+function updateSummaryPlanPrice() {
+  const summaryPlanContainer = document.querySelector(".summary-step");
+  const summaryPlanPrice = summaryPlanContainer.querySelector(".plan-price");
+  const summaryExtraService = summaryPlanContainer.querySelector(".service");
+  const summaryExtraStorage = summaryPlanContainer.querySelector(".storage");
+  const summaryExtraProfile = summaryPlanContainer.querySelector(".profile");
+
+  if (summaryPlanPrice) {
+    const planType = selectedPlan;
+    const billingFrequency = isYearlyBilling ? "yr" : "mo";
+    const price = isYearlyBilling
+      ? priceMapping[planType] * 10
+      : priceMapping[planType];
+    summaryPlanPrice.textContent = isNaN(price)
+      ? "Select a plan"
+      : `$${price.toFixed(2)} / ${billingFrequency}`;
+  }
+
+  if (summaryExtraService) {
+    summaryExtraService.textContent = isYearlyBilling ? "+$10/yr" : "+$1/mo";
+  }
+
+  if (summaryExtraStorage) {
+    summaryExtraStorage.textContent = isYearlyBilling ? "+$20/yr" : "+$2/mo";
+  }
+
+  if (summaryExtraProfile) {
+    summaryExtraProfile.textContent = isYearlyBilling ? "+$20/yr" : "+$2/mo";
+  }
+}
+
+function updateTotalPrice() {
+  let totalAmount = 0;
+
+  plans.forEach((plan) => {
+    if (plan.classList.contains("selected")) {
+      const planType = plan.classList[0];
+      totalAmount += isYearlyBilling
+        ? priceMapping[planType] * 10
+        : priceMapping[planType];
+    }
+  });
+
+  const addOns = document.querySelectorAll(".add-on");
+  addOns.forEach((addOn) => {
+    const checkBox = addOn.querySelector(`input[type="checkbox"]`);
+    if (checkBox.checked) {
+      const baseAddonPriceElement = addOn.querySelector(".extras");
+      const baseAddonPrice = parseFloat(
+        baseAddonPriceElement.dataset.basePrice
+      );
+
+      if (!isNaN(baseAddonPrice)) {
+        const billingFrequencyText = isYearlyBilling ? "/yr" : "/mo";
+        const updatedAddonPrice = isYearlyBilling
+          ? baseAddonPrice * 10
+          : baseAddonPrice;
+        baseAddonPriceElement.textContent = `+$${updatedAddonPrice}${billingFrequencyText}`;
+
+        totalAmount += isYearlyBilling ? baseAddonPrice * 10 : baseAddonPrice;
+      }
+    }
+  });
+
+  const totalPriceElement = document.querySelector(".total-amount");
+  if (totalPriceElement) {
+    totalPriceElement.textContent = isNaN(totalAmount)
+      ? "Select a plan"
+      : isYearlyBilling
+      ? `$${totalAmount.toFixed(2)} / yr`
+      : `$${totalAmount.toFixed(2)} / mo`;
+  }
+}
+
+function updateTotalName() {
+  const totalName = document.querySelector(".summary-step .total-name");
+
+  if (totalName) {
+    totalName.textContent = isYearlyBilling
+      ? "Total (per year)"
+      : "Total (per month)";
+  }
+}
+
+function updateTotalAmount() {
+  const totalAmountElement = document.querySelector(
+    ".summary-step .total-amount"
+  );
+
+  if (totalAmountElement) {
+    let totalAmount = 0;
+
+    plans.forEach((plan) => {
+      if (plan.classList.contains("selected")) {
+        const planType = plan.classList[0];
+        totalAmount += isYearlyBilling
+          ? priceMapping[planType] * 10
+          : priceMapping[planType];
+      }
+    });
+
+    const selectedAddOns = document.querySelectorAll(".add-on input:checked");
+    selectedAddOns.forEach((addOn) => {
+      const extrasPriceElement = addOn.parentNode.querySelector(".extras");
+      const baseExtrasPrice = parseFloat(extrasPriceElement.dataset.basePrice);
+      const formattedExtrasPrice = isYearlyBilling
+        ? baseExtrasPrice * 10
+        : baseExtrasPrice;
+
+      if (isYearlyBilling) {
+        extrasPriceElement.textContent = `+$${formattedExtrasPrice.toFixed(
+          2
+        )} / yr`;
+      }
+
+      totalAmount += isYearlyBilling
+        ? formattedExtrasPrice * 10
+        : formattedExtrasPrice;
+    });
+
+    totalAmountElement.textContent = isNaN(totalAmount)
+      ? "Select a plan"
+      : `$${totalAmount.toFixed(2)}`;
+  }
+}
+
+const addOns = document.querySelectorAll(".add-on");
+
+addOns.forEach((addOn, index) => {
+  const checkBox = addOn.querySelector(`input[type="checkbox"]`);
+  checkBox.addEventListener("change", function () {
+    const selectedAddonElement = document.getElementById(
+      `selectedAddon${index + 1}`
+    );
+
+    if (checkBox.checked) {
+      addOn.classList.add("picked");
+      if (selectedAddonElement) {
+        selectedAddonElement.style.display = "flex";
+        const addonLabel = addOn.querySelector(".text-title").textContent;
+        const addonPrice = addOn.querySelector(".extras").textContent.trim();
+        updateAddOnSummary(addonLabel, addonPrice);
+      }
     } else {
-      priceElement.textContent = `$${priceMapping[planType]}/mo`;
-      freeMonthsElement.textContent = "2 months free";
+      addOn.classList.remove("picked");
+      if (selectedAddonElement) {
+        selectedAddonElement.style.display = "none";
+        const existingAddon = document.querySelector(
+          `.extra-picks:contains('${selectedAddonElement.textContent.trim()}')`
+        );
+        if (existingAddon) {
+          existingAddon.remove();
+        }
+      }
+    }
+
+    if (isYearlyBilling) {
+      updateTotalPrice();
     }
   });
 });
 
-const addOns = document.querySelectorAll(".add-on");
-
 addOns.forEach((addOn) => {
   const checkBox = addOn.querySelector(`input[type="checkbox"]`);
-  checkBox.addEventListener("change", function () {
-    if (checkBox.checked) {
-      addOn.classList.add("picked");
-    } else {
-      addOn.classList.remove("picked");
-    }
-  });
+  checkBox.addEventListener("change", updateTotalPrice);
 });
 
 function updateStepDisplay() {
@@ -203,7 +397,14 @@ function handleInputValidation() {
   nextButton.disabled = !isValid;
 }
 
+function updateSummaryStep() {
+  updateSummaryPlan();
+  updateTotalName();
+  updateTotalAmount();
+}
+
 nextButton.addEventListener("click", nextStep);
 backButton.addEventListener("click", prevStep);
 
 updateStepDisplay();
+updateSummaryStep();
